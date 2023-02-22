9f167964bbfeaeb66ede425a84089e881640acd6
Ritik Raj <54058176+Ritik8141@users.noreply.github.com>
2023-02-22 11:45:11 +0530

    Add files via upload

---
+++ BankManganementSystem/src/main/java/com/mycompany/bankmanganementsystem/Account.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.bankmanganementsystem;
+
+/**
+ *
+ * @author acer
+ */
+
+public class Account 
+{
+    private double balance;
+    private long accNum;
+    private String accHolder;
+     
+     public Account(String name, long num)
+     {
+    	 accHolder = name;
+         accNum = num;
+         balance = 0.00;
+    	 System.out.println("Account Created");
+     }
+     
+     public void deposit(double amt)
+     {
+    	 balance = balance + amt;
+    	 System.out.println("Amount Deposited.");
+     }
+     
+     public void withdraw(double amt) throws BankAccountException
+     {
+    	 try
+    	 { 
+    	    if(balance >= amt)
+    	    {
+    		    balance = balance - amt;
+        	    System.out.println("Amount Withdrawn");
+    	    }
+    	    else
+    	    {
+    		    throw new BankAccountException("Could not Withdraw: Insufficent Amount.");
+    	    }
+    	 }
+    	 catch(BankAccountException bae)
+    	 {
+              bae.printStackTrace();
+         }
+     }
+     
+     public void printBalance()
+     {
+    	 System.out.println("Current balance: " + balance);    	 
+     }
+     public double getBalance()
+     {
+         return balance;
+     }
+}
+
---
+++ BankManganementSystem/src/main/java/com/mycompany/bankmanganementsystem/BankAccountException.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.bankmanganementsystem;
+
+/**
+ *
+ * @author acer
+ */
+class BankAccountException extends Exception
+{
+	public BankAccountException()
+        {
+		super();
+	}
+	public BankAccountException(String message)
+	{
+		super(message);
+	}
+}
